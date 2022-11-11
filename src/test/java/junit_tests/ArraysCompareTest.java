5269f580f626bdcbbb5641ee39e2cd1bca303b4e
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-11 16:31:47 +0200

    Step 08

---
+++ src/test/java/junit_tests/ArraysCompareTest.java
@@ -0,0 +1,18 @@
+package junit_tests;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ArraysCompareTest {
+    //Arrays.sort
+    @Test
+    public void testArraySort_RandomArray() {
+        int [] numbers = {12, 3, 4, 1};
+        int [] expected = {1, 3, 4, 12};
+        Arrays.sort(numbers);
+        assertArrayEquals(expected, numbers);
+    }
+}
