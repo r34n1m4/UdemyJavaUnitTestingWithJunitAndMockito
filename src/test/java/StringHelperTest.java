5053170f0a34bbf3051bbb3b9a5b25cd0b61da3f
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-11 15:51:27 +0200

    Innitial commit

--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -4,10 +4,28 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringHelperTest {
+    //AACD => CD; ACD => CD; CDEF => CDEF; CDAA => CDAA;
+
+    StringHelper helper = new StringHelper();
+
     @Test
-    public void test() {
+    public void testTruncateAInFirst2Positions_AinFrist2Positions() {
         StringHelper helper = new StringHelper();
         assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions_AinFristPosition() {
         assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions_noA() {
+        assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+    }
+
+    @Test
+    public void testTruncateAInFirst2Positions_AinLastPositions() {
+        assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
     }
 }
