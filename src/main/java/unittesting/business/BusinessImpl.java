60f2434c2b8ccd7fd4c453405b09567ea94f5281
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-21 11:33:52 +0200

    Master Mockito step02

---
+++ src/main/java/unittesting/business/BusinessImpl.java
@@ -0,0 +1,11 @@
+package unittesting.business;
+
+public class BusinessImpl {
+    public int calculateSum(int[] data) {
+        int sum = 0;
+        for(int value:data) {
+            sum += value;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/unittesting/business/BusinessImplTest.java
@@ -0,0 +1,16 @@
+package unittesting.business;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BusinessImplTest {
+
+    @Test
+    public void calculateSum_basic() {
+        BusinessImpl business = new BusinessImpl();
+        int actualResult = business.calculateSum(new int[]{1, 2, 3});
+        int expectedResult = 6;
+        assertEquals(expectedResult, actualResult);
+    }
+}
