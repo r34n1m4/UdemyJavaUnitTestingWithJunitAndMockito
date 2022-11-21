0af490387fe556240c7107a51e083ac78ce317c4
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-21 15:30:38 +0200

    Master Mockito step14

--- src/test/java/unittesting/business/ListMockTest.java
+++ src/test/java/unittesting/business/ListMockTest.java
@@ -3,6 +3,7 @@ package unittesting.business;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -75,5 +76,34 @@ public class ListMockTest {
         List<String> allValues = captor.getAllValues();
         assertEquals("SomeString1", allValues.get(0));
         assertEquals("SomeString2", allValues.get(1));
+    }
+
+    @Test
+    public void mocking() {
+        ArrayList arrayListMock = mock(ArrayList.class);
+        System.out.println(arrayListMock.get(0));//null
+        System.out.println(arrayListMock.size());//0
+        arrayListMock.add("Test1");
+        arrayListMock.add("Test2");
+        System.out.println(arrayListMock.size());//0
+        when(arrayListMock.size()).thenReturn(5);
+        System.out.println(arrayListMock.size());//5
+    }
+
+    @Test
+    public void spying() {
+        ArrayList arrayListSpy = spy(ArrayList.class);
+        arrayListSpy.add("Test0");
+        System.out.println(arrayListSpy.get(0));//Test0
+        System.out.println(arrayListSpy.size());//1
+        arrayListSpy.add("Test1");
+        arrayListSpy.add("Test2");
+        System.out.println(arrayListSpy.size());//3
+
+        when(arrayListSpy.size()).thenReturn(5);
+        System.out.println(arrayListSpy.size());//5
+
+        arrayListSpy.add("Test3");
+        System.out.println(arrayListSpy.size());//5
     }
 }
