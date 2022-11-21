b0acb7aa7e7390f3aeab5007f013f3c16725f1d6
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-21 15:19:38 +0200

    Master Mockito step13

--- src/test/java/unittesting/business/ListMockTest.java
+++ src/test/java/unittesting/business/ListMockTest.java
@@ -62,4 +62,18 @@ public class ListMockTest {
 
         assertEquals("SomeString", captor.getValue());
     }
+
+    @Test
+    public void multipleArgumentCapturing() {
+        //SUT
+        mock.add("SomeString1");
+        mock.add("SomeString2");
+        //verification
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock, times(2)).add(captor.capture());
+
+        List<String> allValues = captor.getAllValues();
+        assertEquals("SomeString1", allValues.get(0));
+        assertEquals("SomeString2", allValues.get(1));
+    }
 }
