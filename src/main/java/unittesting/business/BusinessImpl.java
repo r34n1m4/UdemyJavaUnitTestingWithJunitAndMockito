30a4e82d6e617c21a7f6a3d921b23f1ff1ef4345
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-21 13:43:45 +0200

    Master Mockito step03

--- src/main/java/unittesting/business/BusinessImpl.java
+++ src/main/java/unittesting/business/BusinessImpl.java
@@ -1,8 +1,25 @@
 package unittesting.business;
 
+import unittesting.data.DataService;
+
 public class BusinessImpl {
+    private DataService dataService;
+
+    public void setDataService(DataService dataService) {
+        this.dataService = dataService;
+    }
+
     public int calculateSum(int[] data) {
         int sum = 0;
+        for(int value:data) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    public int calculateSumUsingDataService() {
+        int sum = 0;
+        int[] data = dataService.retrieveAllData();
         for(int value:data) {
             sum += value;
         }
---
+++ src/main/java/unittesting/data/DataService.java
@@ -0,0 +1,5 @@
+package unittesting.data;
+
+public interface DataService {
+    int[] retrieveAllData();
+}
