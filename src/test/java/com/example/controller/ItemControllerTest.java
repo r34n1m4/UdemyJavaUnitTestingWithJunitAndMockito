5e98cafc34393d935488f679ffb5171535154ece
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-22 18:04:55 +0200

    Step 08

--- src/test/java/com/example/controller/ItemControllerTest.java
+++ src/test/java/com/example/controller/ItemControllerTest.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
 
+import com.example.business.ItemBusinessService;
+import com.example.model.Item;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,6 +15,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,6 +25,9 @@ public class ItemControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    private ItemBusinessService businessService;
 
     @Test
     public void dummyItem_basic() throws Exception {
@@ -32,6 +39,21 @@ public class ItemControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content()
                         .json("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
+                .andReturn();
+    }
+
+    @Test
+    public void itemFromBusinessService_basic() throws Exception {
+
+        when(businessService.retrieveHardcodedItem())
+                .thenReturn(new Item(2, "Item", 10,10));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/item-from-business-service")
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{id:2,name:Item,price:10,quantity:10}"))
                 .andReturn();
     }
 }
