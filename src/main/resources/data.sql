c64cf37ebac2fedbd56bf8a6634d2112b4642ca1
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-22 20:56:00 +0200

    Step 11

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-insert into item
-values(10001, 'Item1', 10,20);
+insert into item values(10001, 'Item1', 10,10);
+insert into item values(10002, 'Item2', 20,20);
+insert into item values(10003, 'Item3', 30,30);
