8505fd1e0e203bb7bde8b14a81b0d00b0933caf0
r34n1m4 <vadimzuravskis@gmail.com>
2022-11-22 17:45:13 +0200

    Step 07

--- src/main/java/com/example/controller/ItemController.java
+++ src/main/java/com/example/controller/ItemController.java
@@ -1,14 +1,24 @@
 package com.example.controller;
 
+import com.example.business.ItemBusinessService;
 import com.example.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
 
+    @Autowired
+    private ItemBusinessService businessService;
+
     @GetMapping("/dummy-item")
     public Item dummyItem() {
         return new Item(1, "Ball", 10, 100);
+    }
+
+    @GetMapping("/item-from-business-service")
+    public Item itemFromBusinessService() {
+        return businessService.retrieveHardcodedItem();
     }
 }
